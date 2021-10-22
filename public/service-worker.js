87aa22f921f0ec834020f18afcd7979420d5f6ea
Isaac Aguirre <isaac.aguirre@du.edu>
2021-10-22 16:10:51 -0600

    Deplying

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,80 +1,77 @@
-const STATIC_CACHE = 'static-cache-v1';
-const RUNTIME_CACHE = 'data-cache-v1';
+console.log("Hello from service worker!")
+
 const FILES_TO_CACHE = [
-    '/',
-    '/index.html',
-    '/index.js',
-    '/indexedDB.js',
-    '/manifest.webmanifest',
-    '/styles.css',
-    '/icons/icon-192x192.png',
-    '/icons/icon-512x512.png'
+    "/",
+    "/index.html",
+    "/styles.css",
+    "/index.js",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
+    "/manifest.webmanifest",
+
 ];
 
-self.addEventListener('install', event => {
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
 
-    event.waitUntil(
-        caches
-            .open(RUNTIME_CACHE)
-            .then(cache => cache.add("/api/transaction"))
+
+self.addEventListener("install", function(evt) {
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            console.log("Your files were pre-cached successfully!");
+            return cache.addAll(FILES_TO_CACHE);
+        })
     );
-    event.waitUntil(
-        caches
-            .open(STATIC_CACHE)
-            .then(cache => cache.addAll(FILES_TO_CACHE))
-    );
+
     self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches
-            .keys()
-            .then(keyList => {
-                return Promise.all(
-                    keyList.map(key => {
-                        if (key !== STATIC_CACHE && key !== RUNTIME_CACHE) {
-                            console.log("Old cache data removed");
-                            return caches.delete(key);
-                        }
-                    })
-                );
-            })
+self.addEventListener("activate", function(evt) {
+    evt.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log("Removing old cache data", key);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
     );
 
-    self.clients.claim()
+    self.clients.claim();
 });
 
-self.addEventListener('fetch', event => {
-    if (event.request.url.includes("/api/transaction")) {
-        event.respondWith(
-            caches
-                .open(RUNTIME_CACHE)
-                .then(cache => {
-                    return fetch(event.request)
-                        // clone if successful
-                        .then(response => {
-                            if (response.status === 200) {
-                                cache.put(event.request.url, response.clone());
-                            }
-                            return response;
-                        })
-                        .catch(err => cache.match(event.request));
-                })
-                .catch(err => console.log(err))
+
+self.addEventListener("fetch", function(evt) {
+
+    if (evt.request.url.includes("/api/")) {
+        evt.respondWith(
+            caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(evt.request)
+                    .then(response => {
+
+                        if (response.status === 200) {
+                            cache.put(evt.request.url, response.clone());
+                        }
+
+                        return response;
+                    })
+                    .catch(err => {
+
+                        return cache.match(evt.request);
+                    });
+            }).catch(err => console.log(err))
         );
+
         return;
     }
-    event.respondWith(
-        caches
-            .open(STATIC_CACHE)
-            .then(cache => {
-                return cache
-                    .match(event.request)
-                    .then(response => {
-                        return response || fetch(event.request);
-                    });
-            })
-            .catch(err => console.log(err))
+
+
+    evt.respondWith(
+        caches.match(evt.request).then(function(response) {
+            return response || fetch(evt.request);
+        })
     );
 });
